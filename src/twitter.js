c579bd115a69a98297b6e491f27ca0dfb1e24139
Benjamin Koltes <ayc0.benj@gmail.com>
2023-08-03 01:15:29 +0200

    Update twitter.js

--- src/twitter.js
+++ src/twitter.js
@@ -1,5 +1,6 @@
 const hideAdd = () =>
     Array.from(document.querySelectorAll('article'))
-        .filter((e) => e.textContent.includes('Promoted'))
+        .filter((e) => e.textContent.includes('Promoted') || e.querySelector(':has(svg[aria-label="Verified account"])'))
         .forEach((e) => (e.style = 'display: none'));
+
 window.addEventListener('scroll', hideAdd);
